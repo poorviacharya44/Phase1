0926141b81ce68924a4d5fd51becb93324bc5c3a
Poorvi <poorviacharya72@gmail.com>
2022-03-04 16:27:35 +0530

    done

---
+++ EmpId_2500928_Poorvi_R/Phase1Projects/PracticeProject-LongestInSubsequence/src/lis/LIS.java
@@ -0,0 +1,69 @@
+package lis;
+
+public class LIS{
+static int LI_Subseq(int array[], int arr_len){
+    int arr[] = new int[arr_len];
+    int i, j, max = 0;
+    for (i = 0; i < arr_len; i++)
+       arr[i] = 1;
+    for (i = 1; i < arr_len; i++)
+    for (j = 0; j < i; j++)
+    if (array[i] > array[j] && arr[i] < arr[j] + 1)
+    arr[i] = arr[j] + 1;
+    for (i = 0; i < arr_len; i++)
+    if (max < arr[i])
+    max = arr[i];
+    return max;
+ }
+ public static void main(String args[]){
+	 int array[] = {5,4,1,2,3};
+    //int array[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+    int arr_len = array.length;
+    System.out.println("The length of the longest increasing subsequence is " +  LI_Subseq(array, arr_len));
+ }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
