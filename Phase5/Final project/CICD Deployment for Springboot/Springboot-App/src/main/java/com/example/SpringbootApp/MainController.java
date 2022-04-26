ea595e4a741a7a76f4bec8e7fc01898f37debc19
Poorvi <poorviacharya72@gmail.com>
2022-04-26 19:03:56 +0530

    done

---
+++ Phase5/Final project/CICD Deployment for Springboot/Springboot-App/src/main/java/com/example/SpringbootApp/MainController.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "index.html";
+  }
+}
