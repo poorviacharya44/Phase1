d8e318eea8848fdaa5cb189b7df4be3f614d9c33
Poorvi <poorviacharya72@gmail.com>
2022-04-27 18:37:49 +0530

    done

---
+++ Phase5/Practice Projects/5.Deploying ELK Stack on Docker Container/src/test/java/com/example/ELKstack/DeployingElKstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ELKstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployingElKstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
